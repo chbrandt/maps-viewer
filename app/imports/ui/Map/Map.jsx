c1bb1f8c852cd6a32b7f70323a2e308453484549
Carlos H Brandt <c.brandt@jacobs-university.de>
2019-05-06 17:44:46 +0200

    [wip] moving out from react

--- app/imports/ui/Map/Map.jsx
+++ app/imports/ui/Map/Map.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Session } from 'meteor/session';
 import { Tracker } from 'meteor/tracker';
 
-import proj4 from 'proj4';
-
 // Load basemaps and overlays definitions
 import { baseMaps, overlayMaps } from '/imports/api/basemaps.js';
 
@@ -16,6 +14,8 @@ export default class Map extends React.Component {
     if (bodiesList.indexOf(this.props.body) == -1) {
       throw "ValueError: was expecting 'props.body' to be among: " + JSON.stringify(bodiesList);
     }
+
+    this.setMap = this.setMap.bind(this);
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -26,11 +26,16 @@ export default class Map extends React.Component {
   }
 
   render() {
-    return <div id='map' style={{width:'100%', height:'100%'}}/>;
+    return (
+      <div>
+        <div id='map' style={{width:'100%', height:'100%'}}/>
+        <div id='nodelist'/>
+      </div>
+    )
   }
 
   componentDidMount() {
-    this.map = setMap(this.props.body);
+    this.map = this.setMap(this.props.body);
     // this.map.panTo([10,100]);
 
     // Tracker.autorun(() => {
@@ -48,155 +53,139 @@ export default class Map extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    // this.map.off();
-    // this.map.remove();
     this.map.setTarget(null);
     this.map = null;
-    this.map = setMap(this.props.body);
+    this.map = this.setMap(this.props.body);
   }
-}
 
-function setMap(body) {
-  // proj4.defs('EPSG:999999',
-  //   '+proj=merc +a=2439700 +b=2439700 ' +
-  //   '+lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 ' +
-  //   '+units=m +nadgrids=@null +wktext +no_defs');
-  // ol.proj.proj4.register(proj4);
-  // const mercury_mercator = ol.proj.get('EPSG:999999');
-
-  // var vectorSource = new ol.source.Vector({
-  //   format: new ol.format.GeoJSON(),
-  //   url: function(extent) {
-  //     return 'http://localhost:8080/mercury/wfs?service=WFS&' +
-  //         'version=1.1.0&request=GetFeature&' +
-  //         'typename=mercury:H05_geological_units_3_classes-MERCATOR&' +
-  //         'outputFormat=application/json&srsname=EPSG:3857&' +
-  //         'bbox=' + extent.join(',');
-  //   },
-  //   strategy: ol.loadingstrategy.bbox,
-  //   crossOrigin: null
-  // });
-  //
-  // var vector = new ol.layer.Vector({
-  //   source: vectorSource,
-  //   style: new ol.style.Style({
-  //     stroke: new ol.style.Stroke({
-  //       color: 'rgba(0, 0, 255, 1.0)',
-  //       width: 2
-  //     })
-  //   })
-  // });
-
-  // var wmsSource = new ol.source.ImageWMS({
-  //   url: 'http://localhost:8080/mercury/wms',
-  //   params: {'LAYERS': 'mercury:hokusai_450_mpx_Band1of8_255-MERCATOR'},
-  //   serverType: 'geoserver',
-  //   crossOrigin: null
-  // });
-  //
-  // var wmsLayer = new ol.layer.Image({
-  //   source: wmsSource
-  // });
-
-  var xyzSource = new ol.source.XYZ({
-    url: "http://localhost:8080/gwc/service/tms/1.0.0" +
-         "mercury%3Amercury_global_color_665m_EPSG4326@EPSG%3A4326@jpeg/{z}/{x}/{-y}.jpg",
+setMap(body) {
+  var style_simple = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#00AAFF',
+      width: 0.1
+    })
   });
 
-  var xyzLayer = new ol.layer.Tile({
-    source: xyzSource
+  var style_selected = new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255,255,255,0.5)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#FFAA00',
+      width: 1
+    })
+  });
+
+  var mousePositionControl = new ol.control.MousePosition({
+          className: 'custom-mouse-position',
+          target: document.getElementById('location'),
+          coordinateFormat: ol.coordinate.createStringXY(5),
+          undefinedHTML: '&nbsp;'
+        });
+
+  var format = 'image/png';
+
+  var raster_global = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url: 'http://localhost:8080/mercury/wms',
+      params: {LAYERS: 'mercury:mercury_global_MD3Color_665m_EPSG4326'}
+    })
+  });
+
+  var raster_geounits_3 = new ol.layer.Tile({
+    visible: true,
+    opacity: 0.5,
+    source: new ol.source.TileWMS({
+      url: 'http://localhost:8080/mercury/wms',
+      params: {'FORMAT': format,
+               'VERSION': '1.1.1',
+               tiled: true,
+            "LAYERS": 'mercury:H05_geological_units_3_classes_EPSG4326',
+            "exceptions": 'application/vnd.ogc.se_inimage',
+      }
+    })
+  });
+
+  var vector_geounits_3_src = new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: function(extent) {
+      return 'http://localhost:8080/wfs?' +
+          'service=WFS&version=1.1.0&request=GetFeature&' +
+          'typename=mercury:H05_geological_units_3_classes_EPSG4326&' +
+          'outputFormat=application/json&' +
+          'srsname=EPSG:4326&' +
+          'bbox=' + extent.join(',');
+    },
+    strategy: ol.loadingstrategy.bbox,
+    crossOrigin: null
+  });
+
+  var vector_geounits_3 = new ol.layer.Vector({
+    source: vector_geounits_3_src,
+    style: style_simple
   });
 
   var map = new ol.Map({
-    layers: [xyzLayer,wmsLayer,vector],
     target: 'map',
     view: new ol.View({
-      // projection: mercury_mercator,
-      // center: ol.proj.fromLonLat([0,0]),
-      center: [0,0],
+      projection: 'EPSG:4326',
+      center: [0, 0],
       zoom: 2
-    })
+    }),
+    controls: ol.control.defaults().extend([mousePositionControl]),
+    layers: [
+      raster_global,
+      raster_geounits_3,
+      vector_geounits_3,
+    ]
   });
-  // var bmSet = {}
-  // var bm;
-  // baseMaps[body].forEach((pars) => {
-  //   if (pars.wms || pars.service == 'wms') {
-  //     bm = new ol.layer.Tile({
-  //       source: new ol.source.WMTS({
-  //         ...pars.options
-  //       })
-  //     })
-  //   } else if (pars.service == 'tms'){
-  //     bm = new ol.layer.Tile({
-  //       source: new ol.source.XYZ({
-  //         ...pars.options
-  //       })
-  //     })
-  //   }
-  //   bmSet[pars.label] = bm;
-  // });
-  // map.addLayer(bm)
-  // bm.addTo(map);
 
-  //
-  // var omSet = {}
-  // var om;
-  // if (overlayMaps[body]) {
-  //   overlayMaps[body].forEach((pars) => {
-  //     if (pars.wms) {
-  //       console.log(pars);
-  //       om = new ol.layer.Tile({
-  //         source: new ol.source.TileWMS({
-  //           url: pars.url,
-  //           params: pars.params,
-  //           serviceType: pars.serviceType
-  //         })
-  //       })
-  //     } else {
-  //       om = new ol.layer.Tile({
-  //         source: new ol.source.XYZ({
-  //           url: pars.url
-  //         })
-  //       })
-  //     }
-  //     omSet[pars.label] = om;
-  //     map.addLayer(om)
-  //   })
-  // }
-  //
-  // L.control.layers(bmSet, omSet, {position: 'topright'}).addTo(map);
-  //
-  // var options_ = {interval: 20,
-  //              showOriginLabel: true,
-  //              redraw: 'move',
-  //              zoomIntervals: [
-  //               {start: 0, end: 3, interval: 50},
-  //               {start: 4, end: 5, interval: 5},
-  //               {start: 6, end: 20, interval: 1}
-  //           ]};
-  // L.simpleGraticule(options_).addTo(map);
-  //
-  // /*
-  //   Connect any map "move" event to the update of global 'bbox' variable.
-  //   The 'bbox' variable is being watched by the connection with the MongoDB,
-  //   at the App's sbuscription point to 'data_geo' Collection.
-  //   (see App.js/withTracker())
-  //   */
-  // map.on('moveend', (event) => {
-  //   let bounds = map.getBounds();
-  //   let bbox = [
-  //     [bounds.getWest(), bounds.getSouth()],
-  //     [bounds.getEast(), bounds.getNorth()]
-  //   ];
+  map.on('singleclick', function(evt) {
+    document.getElementById('nodelist').innerHTML = "Loading... please wait...";
+    var view = map.getView();
+    var viewResolution = view.getResolution();
+    var source = raster_geounits_3.getSource();
+    var url = source.getGetFeatureInfoUrl(
+      evt.coordinate, viewResolution, view.getProjection(),
+      {'INFO_FORMAT': 'text/html', 'FEATURE_COUNT': 50});
+    if (url) {
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        document.getElementById('nodelist').innerHTML = this.responseText;
+      }
+      xhr.open("GET", url);
+      xhr.send();
+    }
+  });
+
+  // a normal select interaction to handle click
+  var select = new ol.interaction.Select({style: style_selected});
+  map.addInteraction(select);
+
+  this.setState({selectedFeatures: select.getFeatures()});
+
+  // a DragBox interaction used to select features by drawing boxes
+  var dragBox = new ol.interaction.DragBox({
+    condition: ol.events.condition.platformModifierKeyOnly
+  });
+
+  map.addInteraction(dragBox);
+
+  dragBox.on('boxend', function() {
+    var extent = dragBox.getGeometry().getExtent();
+    var selectedFeatures = this.state.selectedFeatures.copy();
+    // selectedFeatures.clear();
+    vector_geounits_3_src.forEachFeatureInExtent(extent, function(feature) {
+      selectedFeatures.push(feature);
+    });
+    this.setState({selectedFeatures: selectedFeatures});
+  });
+
+  // clear selection when drawing a new box and when clicking on the map
+  // dragBox.on('boxstart', function() {
+  //   this.state.selectedFeatures.clear();
   // });
-  //
-  // var zoombox = L.control.infobox({position:'bottomleft'}).addTo(map);
-  //
-  // map.on('zoomend', (event) => {
-  //   var zoom = map.getZoom();
-  //   zoombox.setValue(zoom);
-  // })
-  //
-  // We will need the 'map' in other moments of the Component's life-cycle..
+
   return map;
+}
 }
