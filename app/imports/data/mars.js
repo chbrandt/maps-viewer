cae6b3e6c698d4c5a2db5925f3eacbb5aa996283
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-02-25 17:29:59 +0100

    [wip] Add latest packages to data set

--- app/imports/data/mars.js
+++ app/imports/data/mars.js
@@ -38,6 +38,36 @@ export var mars = {
           typename: 'mars:PM-MAR-MS-Arsinoes_01'
         }
       }
+    },
+    {
+      "name": "C-Arsinoes",
+      "pm_id": "PM-MAR-C-Arsinoes",
+      "center": {
+        "lon": -28.354948058817456,
+        "lat": -13.0749921424451
+      },
+      "bbox": {
+        "xmin": -28.469381174203818,
+        "xmax": -28.240514943431094,
+        "ymin": -13.169841214236444,
+        "ymax": -12.980143070653755,
+        "srs": "EPSG:4326"
+      }
+    },
+    {
+      "name": "C-Crommelin",
+      "pm_id": "PM-MAR-C-Crommelin",
+      "center": {
+        "lon": -10.309911318473496,
+        "lat": 7.279592356400499
+      },
+      "bbox": {
+        "xmin": -10.844780922334394,
+        "xmax": -9.775041714612598,
+        "ymin": 4.39276139195179,
+        "ymax": 10.166423320849209,
+        "srs": "EPSG:4326"
+      }
     }
   ]
 };
